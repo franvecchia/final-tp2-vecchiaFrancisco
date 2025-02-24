c887445dc0da964b0192191de5b5feda4e51e537
Francisco Vecchia <franvecchia00@gmail.com>
2025-02-24 20:41:47 -0300

    validacion sobre monto

--- models/Donacion.js
+++ models/Donacion.js
@@ -4,7 +4,10 @@ export default class Donacion {
             throw new Error("El monto no puede ser nulo")
         } else if (monto <=0) {
             throw new Error("El monto debe ser mayor a 0")
+        } else if (typeof monto !== "number") {
+            throw new Error("El monto debe ser un numero valido")
         }
+        
         this.id = id
         this.monto = monto
         this.nombreCompleto = !nombreCompleto ? "Anonimo" : nombreCompleto 
